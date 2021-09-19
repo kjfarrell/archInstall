d196e5b74bdf14d3ba1c098e09da012aa302e663
kjfarrell <87602960+kjfarrell@users.noreply.github.com>
2021-09-20 06:05:17 +0800

    Update installArchChrooted.sh

--- installArchChrooted.sh
+++ installArchChrooted.sh
@@ -18,4 +18,4 @@ echo "#### MKINITCPIO ####"
 echo "#### SET ROOT PASSWORD ####"
 echo "root:"$passvar | chpasswd
 pacman -S grub os-prober --noconfirm
-grub-install --target=i386-pc /dev/nvme0n1
+grub-install --target=x86_64-efi --efi-directory=esp --boot-directory=esp --bootloader-id=GRUB
