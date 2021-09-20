10222a7af4dbacd644d996627144155892a52e12
kjfarrell <87602960+kjfarrell@users.noreply.github.com>
2021-09-20 08:17:16 +0800

    Update installArch.sh

--- installArch.sh
+++ installArch.sh
@@ -13,7 +13,7 @@ echo "#### Partitioning ####"
 # Partition HD
 wipefs -a $TARGET_DISK
 sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk $TARGET_DISK
-  o # clear the in memory partition table
+  g # clear the in memory partition table
   n # new partition
   p # primary partition
   1 # partition number 1
